c168a73152d644a3062cef2cc9f65ca41faafe53
Tim Hargreaves <timhargreaves@jag.net>
2014-01-24 21:38:41 -0800

    EmotionTracker
    Added empty script

---
+++ Assets/EmotionMonitor.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EmotionMonitor : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
