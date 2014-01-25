86055d22b72546764caef585dfac5d9483dc291a
Tim Hargreaves <timhargreaves@jag.net>
2014-01-25 10:55:49 -0800

    Player movement and input
    Added left and right player movement. Moved movement code to its own
    script.

---
+++ Assets/PlayerMove.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMove : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate () {
+
+		rigidbody2D.AddForce(new Vector3(Input.GetAxis("Horizontal") * 10.0f, 0.0f));
+		//Debug.Log(Input.GetAxis("Horizontal"));
+	}
+}
