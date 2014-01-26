d2c8fc90cac1113a69e4bc7b7b6d2e5a95c79e15
Tim Hargreaves <timhargreaves@jag.net>
2014-01-26 14:43:59 -0800

    Merging

--- Assets/Scripts/StartButtonScript.cs
+++ Assets/Scripts/StartButtonScript.cs
@@ -5,6 +5,10 @@ public class StartButtonScript : ButtonScript {
 
 	public override void doAction()
 	{
+<<<<<<< HEAD
 		Application.LoadLevel ("Level1");
+=======
+		Application.LoadLevel ("OfficeScene");
+>>>>>>> e40aae054fe0e358e92a14ba9e83579d15b4969c
 	}
 }
