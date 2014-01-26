5ac609b16e31d96da947b50d5e0b4a40172fbcc1
musicaljelly <musicaljelly@gmail.com>
2014-01-25 16:10:00 -0800

    First pass at fillable water body

---
+++ Assets/FillableWaterBody.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FillableWaterBody : MonoBehaviour {
+	
+	Transform spriteTransform;
+	public Vector3 spriteStartScale;
+	 
+	void Start () {
+		spriteTransform = transform.Find("WaterSprite");
+		spriteStartScale = spriteTransform.localScale;
+
+		spriteTransform.localPosition -= new Vector3(0, 3.02f * spriteStartScale.y);
+		spriteTransform.localScale = new Vector3(spriteStartScale.x, 0);
+	}
+
+	
+	void Update () {
+	
+	}
+}
